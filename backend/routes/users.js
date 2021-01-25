3b693c87cce8aaa498fdd502cc61ce16a02d0e94
gmurambadoro <developer.gavin@gmail.com>
2021-01-25 15:06:44 +0200

    Add validation schema + validate user model before persisting

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,60 @@
-const router = require('express').Router;
+const router = require('express').Router();
+const Joi = require('joi');
+
+const User = require('../models/user.model');
+
+const validateUser = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().required().min(3),
+    });
+
+    return schema.validate(data);
+};
+
+// get all users
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err =>res.status(400).json('Error: ' + err))
+    ;
+});
+
+// add a user
+router.route('/').post((req, res) => {
+    const {error, value} = validateUser(req.body);
+
+    if (error) {
+        return res.status(400).send(error);
+    }
+
+    const {username} = value;
+
+    const user = new User({ username });
+
+    user.save()
+        .then((data) => {
+            console.log('User added', data);
+
+            return res.json(data);
+        })
+        .catch(err => res.status(400).json(String(err).toString()))
+    ;
+});
+
+// retrieve a single user
+router.route('/:username').get((req, res) => {
+    const {username} = req.params;
+
+    User.findOne({ username: username })
+        .then(data => {
+            if (!data) {
+                return res.status(404).json('There is no user by this username.');
+            }
+
+            return res.json(data);
+        })
+        .catch((err) => res.status(400).json(String(err).toString()))
+    ;
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const uri = process.env.MONGO_URL;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 })
     .then(() => console.log('MongoDB database connection established successfully'))
     .catch(err => console.log(err));
